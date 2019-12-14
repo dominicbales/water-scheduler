e51baa7468b8a7419545e509a1c22f777a58599c
SoteroX <soterox13@gmail.com>
2019-12-13 21:09:23 -0600

    Updates

---
+++ precache-manifest.8b7364397c8a71ccaa9cd6eebd2f6f1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39b48d48659fb7f7019ac383d688b00b",
+    "url": "/water-scheduler/index.html"
+  },
+  {
+    "revision": "30e50d37d82b5e879a4a",
+    "url": "/water-scheduler/static/css/2.25b83785.chunk.css"
+  },
+  {
+    "revision": "a07d706ab4283331436f",
+    "url": "/water-scheduler/static/css/main.408db6df.chunk.css"
+  },
+  {
+    "revision": "30e50d37d82b5e879a4a",
+    "url": "/water-scheduler/static/js/2.cb9cd147.chunk.js"
+  },
+  {
+    "revision": "c1b27531d257ffa0742659f09926a161",
+    "url": "/water-scheduler/static/js/2.cb9cd147.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a07d706ab4283331436f",
+    "url": "/water-scheduler/static/js/main.bb9ea339.chunk.js"
+  },
+  {
+    "revision": "958465cefe04dd1ea4b1",
+    "url": "/water-scheduler/static/js/runtime-main.6c5f6aab.js"
+  }
+]);
